51fdef09cfa8770361ee345a3c07b2739a61c96a
Jittapan Pluemsumran <secret@jittapan.app>
2019-09-14 15:09:12 +0700

    Automatic !farm calling

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const tmi = require('tmi.js');
+const fs = require('fs').promises;
 require('dotenv').config();
 
 const opts = {
@@ -10,22 +11,79 @@ const opts = {
     process.env.TWITCH_CHANNEL
   ]
 };
+const LAST_TIMESTAMP_PATH = '.lastFarmTimestamp.json';
+const MAX_DELAY = 2 * 60 * 60 * 1000; // 2 hours
+// const MAX_DELAY = 10000; // 10 sec for testing
 
 const client = new tmi.client(opts);
 
 client.on('connected', connectedHandler);
 client.on('disconnected', disconnectedHandler);
 
-client.connect().then(() => {
-  console.log(`* Connection success.`);
-}).catch(() => {
-  console.log(`* Connection failed.`);
-});
+init();
 
 function connectedHandler(addr, port) {
   console.log(`* Connected to Twitch chat on ${addr}:${port}.`);
+  farm();
 }
 
 function disconnectedHandler(reason) {
   console.log(`* Disconnected from Twitch chat due to ${reason}`);
+  clearTimeout(farmTimeout);
+}
+
+var farmTimeout = undefined; //1568445756913
+
+async function init() {
+  client.connect().then(() => {
+    console.log(`* Connection success.`);
+  }).catch(() => {
+    console.log(`* Connection failed.`);
+  });
+}
+
+async function farm() {
+  let lastFarmCall = await loadTimestamp();
+
+  // Initialize a timeout for !farm calling
+  let delay = MAX_DELAY;
+  const timeSinceLastCall = +new Date() - lastFarmCall;
+  if (timeSinceLastCall > MAX_DELAY) {
+    delay = 0;
+  } else { // Initial call
+    delay = MAX_DELAY - timeSinceLastCall;
+    console.log(`* Will call !farm in ${delay / 1000} seconds.`);
+  }
+  
+  timeout = setTimeout(() => {
+    const ts = +new Date();
+    client.say(process.env.TWITCH_CHANNEL, '!farm');
+    lastFarmCall = ts;
+    saveTimestamp(ts);
+
+    timeout = setTimeout(farm, MAX_DELAY);
+  }, delay);
+}
+
+async function loadTimestamp() {
+  try {
+    const lastFarmTimestampFile = await fs.readFile(LAST_TIMESTAMP_PATH, 'utf8');
+    const lastFarmTimestamp = JSON.parse(lastFarmTimestampFile);
+    console.log(`* Loaded saved !farm timestamp (${lastFarmTimestamp.timestamp}).`);
+    return lastFarmTimestamp.timestamp;
+  } catch {
+    console.log(`* No saved !farm timestamp found. Using current timestamp.`);
+    const ts = 0;
+    await saveTimestamp(ts);
+    return ts;
+  }
+}
+
+async function saveTimestamp(timestamp) {
+  try {
+    await fs.writeFile(LAST_TIMESTAMP_PATH, JSON.stringify({ timestamp }), 'utf8');
+    console.log(`* Saved !farm timestamp (${timestamp}).`);
+  } catch(e) {
+    console.error(e);
+  }
 }
